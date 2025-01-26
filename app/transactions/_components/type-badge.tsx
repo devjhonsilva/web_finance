fc20b37ac680f32799641cf248f33c7039d63160
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-01-26 20:10:36 -0300

    feat: ad transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,68 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.TICKET_FOOD) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Ticket Alimentação
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.TICKET_FUEL) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Ticket Combustível
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesas
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE_TICKET_FOOD) {
+    return (
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesas Ticket Alimentação
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE_TICKET_FUEL) {
+    return (
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesas Ticket Alimentação
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.INVESTMENT) {
+    return (
+      <Badge className="bg-white bg-opacity-10 font-bold text-white">
+        <CircleIcon className="mr-2 fill-white" size={10} />
+        Investimento
+      </Badge>
+    );
+  }
+};
+
+export default TransactionTypeBadge;
