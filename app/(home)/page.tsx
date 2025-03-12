a9e44afb4c714f2dec2e94524b9217ffcae4b913
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-03-12 19:55:48 -0300

    refactor: page screen

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -34,7 +34,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-screen max-h-screen flex-col space-y-6 overflow-hidden p-6">
+      <div className="flex h-screen max-h-screen flex-col space-y-6 p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
@@ -56,7 +56,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
               userCanAddTransaction={userCanAddTransaction}
             />
 
-            <div className="grid h-full min-h-[700px] grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPierChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpenseperCategory}
