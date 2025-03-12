948a8269a04ef3e3c685ede0fe1cacd7f110ec2e
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-03-12 18:52:32 -0300

    refactor: page screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
             baseTheme: dark,
           }}
         >
-          <div className="flex h-full flex-col overflow-hidden">{children}</div>
+          <div className="flex h-full max-h-[1080px] flex-col overflow-hidden">
+            {children}
+          </div>
         </ClerkProvider>
         <Toaster />
       </body>
